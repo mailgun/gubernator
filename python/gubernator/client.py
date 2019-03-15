084d5e2c2ebf3380323bfbc41150fb469873283e
Derrick J. Wippler <thrawn01@gmail.com>
2019-03-15 00:36:57 -0500

    Fixed some issues with python client

--- python/gubernator/client.py
+++ python/gubernator/client.py
@@ -11,12 +11,16 @@ MILLISECOND = 1
 SECOND = MILLISECOND * 1000
 MINUTE = SECOND * 60
 
+OVER_LIMIT = pb.RateLimitResponse.OVER_LIMIT
+UNDER_LIMIT = pb.RateLimitResponse.UNDER_LIMIT
+
 
 class RateLimit(object):
-    def __init__(self, status="", reset_time=0, remaining=0, limit=0):
-        self.status = status
-        self.reset_time = datetime.fromtimestamp(reset_time/1000.0)
+    def __init__(self, status="", reset_ts=0, remaining=0, limit=0):
+        self.reset_time = datetime.utcfromtimestamp(reset_ts / 1000.0)
         self.remaining = remaining
+        self.reset = reset_ts
+        self.status = status
         self.limit = limit
 
     def __str__(self):
@@ -28,7 +32,7 @@ class RateLimit(object):
 
     def sleep_until_reset(self):
         now = datetime.now()
-        time.sleep((self.reset_time-now).seconds)
+        time.sleep((self.reset_time - now).seconds)
 
 
 class Client(object):
@@ -46,18 +50,18 @@ class Client(object):
         req = pb.RateLimitRequestList()
         rate_limit = req.rate_limits.add()
 
+        rate_limit.rate_limit_config.algorithm = algorithm
+        rate_limit.rate_limit_config.duration = duration
+        rate_limit.rate_limit_config.limit = limit
         rate_limit.namespace = namespace
         rate_limit.unique_key = unique
         rate_limit.hits = hits
-        rate_limit.rate_limit_config.limit = limit
-        rate_limit.rate_limit_config.algorithm = algorithm
-        rate_limit.rate_limit_config.duration = duration
 
         resp = self.stub.GetRateLimits(req, timeout=self.timeout)
         rl = resp.rate_limits[0]
         return RateLimit(
-            status=rl.status,
-            reset_time=rl.reset_time,
             remaining=rl.limit_remaining,
+            reset_ts=rl.reset_time,
             limit=rl.current_limit,
+            status=rl.status,
         )

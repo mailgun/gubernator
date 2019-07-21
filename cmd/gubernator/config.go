99ca535b4346c3da954f130f0d5f7bc9a840c4f5
Derrick J. Wippler <thrawn01@gmail.com>
2019-07-20 19:12:30 -0500

    ENV based config

---
+++ cmd/gubernator/config.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"fmt"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	etcd "github.com/coreos/etcd/clientv3"
+	"github.com/mailgun/gubernator"
+	"github.com/mailgun/holster"
+)
+
+type ServerConfig struct {
+	GRPCListenAddress string
+	AdvertiseAddress  string
+	HTTPListenAddress string
+	CacheSize         int
+
+	// Etcd configuration used to find peers
+	EtcdConf etcd.Config
+
+	// Configure how behaviours behave
+	Behaviors gubernator.BehaviorConfig
+}
+
+func confFromEnv() (ServerConfig, error) {
+	var configFile string
+	var conf ServerConfig
+
+	flags := flag.NewFlagSet("gubernator", flag.ContinueOnError)
+	flags.StringVar(&configFile, "config", "", "yaml config file")
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		return conf, err
+	}
+
+	if configFile != "" {
+		log.Infof("Loading env config: %s", configFile)
+		if err := fromEnvFile(configFile); err != nil {
+			return conf, err
+		}
+	}
+
+	// Main config
+	holster.SetDefault(&conf.GRPCListenAddress, os.Getenv("GUBER_GRPC_ADDRESS"), "0.0.0.0:81")
+	holster.SetDefault(&conf.HTTPListenAddress, os.Getenv("GUBER_HTTP_ADDRESS"), "0.0.0.0:80")
+	holster.SetDefault(&conf.AdvertiseAddress, os.Getenv("GUBER_ADVERTISE_ADDRESS"), "127.0.0.1:81")
+	holster.SetDefault(&conf.CacheSize, getEnvInteger("GUBER_CACHE_SIZE"), 50000)
+
+	// Behaviors
+	holster.SetDefault(&conf.Behaviors.BatchTimeout, getEnvDuration("GUBER_BATCH_TIMEOUT"))
+	holster.SetDefault(&conf.Behaviors.BatchLimit, getEnvInteger("GUBER_BATCH_LIMIT"))
+	holster.SetDefault(&conf.Behaviors.BatchWait, getEnvDuration("GUBER_BATCH_WAIT"))
+
+	holster.SetDefault(&conf.Behaviors.GlobalTimeout, getEnvDuration("GUBER_GLOBAL_TIMEOUT"))
+	holster.SetDefault(&conf.Behaviors.GlobalBatchLimit, getEnvInteger("GUBER_GLOBAL_BATCH_LIMIT"))
+	holster.SetDefault(&conf.Behaviors.GlobalSyncWait, getEnvDuration("GUBER_GLOBAL_SYNC_WAIT"))
+
+	// ETCD Config
+	holster.SetDefault(&conf.EtcdConf.Endpoints, getEnvSlice("GUBER_ETCD_ENDPOINTS"), "localhost:2379")
+	holster.SetDefault(&conf.EtcdConf.DialTimeout, getEnvDuration("GUBER_ETCD_DIAL_TIMEOUT"), time.Second*5)
+	holster.SetDefault(&conf.EtcdConf.Username, os.Getenv("GUBER_ETCD_USER"))
+	holster.SetDefault(&conf.EtcdConf.Password, os.Getenv("GUBER_ETCD_PASSWORD"))
+
+	// If env contains any TLS configuration
+	if anyHasPrefix("GUBER_ETCD_TLS_", os.Environ()) {
+		if err := setupTLS(&conf.EtcdConf); err != nil {
+			return conf, err
+		}
+	}
+	return conf, nil
+}
+
+func setupTLS(conf *etcd.Config) error {
+	var tlsCertFile, tlsKeyFile, tlsCAFile string
+
+	// set `GUBER_ETCD_TLS_ENABLE` and this line will
+	// create a TLS config with no config.
+	holster.SetDefault(&conf.TLS, &tls.Config{})
+
+	holster.SetDefault(&tlsCertFile, os.Getenv("GUBER_ETCD_TLS_CERT"))
+	holster.SetDefault(&tlsKeyFile, os.Getenv("GUBER_ETCD_TLS_KEY"))
+	holster.SetDefault(&tlsCAFile, os.Getenv("GUBER_ETCD_TLS_CA"))
+
+	// If the CA file was provided
+	if tlsCAFile != "" {
+		holster.SetDefault(&conf.TLS, &tls.Config{})
+
+		var certPool *x509.CertPool = nil
+		if pemBytes, err := ioutil.ReadFile(tlsCAFile); err == nil {
+			certPool = x509.NewCertPool()
+			certPool.AppendCertsFromPEM(pemBytes)
+		} else {
+			return errors.Wrapf(err, "while loading cert CA file '%s'", tlsCAFile)
+		}
+		holster.SetDefault(&conf.TLS.RootCAs, certPool)
+		conf.TLS.InsecureSkipVerify = false
+	}
+
+	// If the cert and key files are provided attempt to load them
+	if tlsCertFile != "" && tlsKeyFile != "" {
+		tlsCert, err := tls.LoadX509KeyPair(tlsCertFile, tlsKeyFile)
+		if err != nil {
+			return errors.Wrapf(err, "while loading cert '%s' and key file '%s'",
+				tlsCertFile, tlsKeyFile)
+		}
+		holster.SetDefault(&conf.TLS.Certificates, []tls.Certificate{tlsCert})
+	}
+
+	// If no other TLS config is provided this will force connecting with TLS,
+	// without cert verification
+	if os.Getenv("GUBER_ETCD_TLS_SKIP_VERIFY") != "" {
+		holster.SetDefault(&conf.TLS, &tls.Config{})
+		conf.TLS.InsecureSkipVerify = true
+	}
+	return nil
+}
+
+func anyHasPrefix(prefix string, items []string) bool {
+	for _, i := range items {
+		if strings.HasPrefix(i, prefix) {
+			return true
+		}
+	}
+	return false
+}
+
+func getEnvInteger(name string) int {
+	v := os.Getenv(name)
+	if v == "" {
+		return 0
+	}
+	i, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		log.WithError(err).Errorf("while parsing '%s' as an integer", name)
+		return 0
+	}
+	return int(i)
+}
+
+func getEnvDuration(name string) time.Duration {
+	v := os.Getenv(name)
+	if v == "" {
+		return 0
+	}
+	d, err := time.ParseDuration(v)
+	if err != nil {
+		log.WithError(err).Errorf("while parsing '%s' as a duration", name)
+		return 0
+	}
+	return d
+}
+
+func getEnvSlice(name string) []string {
+	v := os.Getenv(name)
+	if v == "" {
+		return nil
+	}
+	return strings.Split(v, ",")
+}
+
+// Take values from a file in the format `GUBER_CONF_ITEM=my-value` and put them into the environment
+// lines that begin with `#` are ignored
+func fromEnvFile(configFile string) error {
+	fd, err := os.Open(configFile)
+	if err != nil {
+		return fmt.Errorf("while opening config file: %s", err)
+	}
+
+	contents, err := ioutil.ReadAll(fd)
+	if err != nil {
+		return fmt.Errorf("while reading config file '%s': %s", configFile, err)
+	}
+	for i, line := range strings.Split(string(contents), "\n") {
+		// Skip comments
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.Split(line, "=")
+		if len(parts) != 2 {
+			return errors.Errorf("malformed key=value on line '%d'", i)
+		}
+
+		if err := os.Setenv(parts[0], parts[1]); err != nil {
+			return errors.Wrapf(err, "while settings environ for '%s=%s'", parts[0], parts[1])
+		}
+	}
+}
--- cmd/gubernator/main.go
+++ cmd/gubernator/main.go
@@ -2,49 +2,22 @@ package main
 
 import (
 	"context"
-	"errors"
-	"flag"
-	"fmt"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
-	"time"
 
-	etcd "github.com/coreos/etcd/clientv3"
-	"github.com/ghodss/yaml"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/mailgun/gubernator"
 	"github.com/mailgun/gubernator/cache"
 	"github.com/mailgun/holster"
-	"github.com/mailgun/holster/clock"
 	"github.com/mailgun/holster/etcdutil"
 	"github.com/sirupsen/logrus"
-	"github.com/smira/go-statsd"
 	"google.golang.org/grpc"
 )
 
 var log = logrus.WithField("category", "server")
 var Version = "dev-build"
-
-type ServerConfig struct {
-	GRPCListenAddress string `json:"grpc-listen-address"`
-	AdvertiseAddress  string `json:"advertise-address"`
-	HTTPListenAddress string `json:"http-listen-address"`
-	CacheSize         int    `json:"cache-size"`
-
-	// Statsd metric configuration
-	StatsdConfig struct {
-		Period   clock.DurationJSON `json:"period"`
-		Endpoint string             `json:"endpoint"`
-		Prefix   string             `json:"prefix"`
-	} `json:"statsd"`
-
-	// Etcd configuration used to find peers
-	EtcdConf etcd.Config `json:"etcd-config"`
-}
 
 func main() {
 	var metrics gubernator.MetricsCollector
@@ -53,11 +26,10 @@ func main() {
 	var conf ServerConfig
 	var err error
 
-	conf, err = loadConfig()
+	// Read our config from the environment or optional environment config file
+	conf, err = confFromEnv()
 	checkErr(err, "while getting config")
 
-	holster.SetDefault(&conf.GRPCListenAddress, "0.0.0.0:81")
-	holster.SetDefault(&conf.AdvertiseAddress, "127.0.0.1:81")
 	opts = []grpc.ServerOption{
 		grpc.MaxRecvMsgSize(1024 * 1024),
 	}
@@ -71,14 +43,14 @@ func main() {
 		Cache:      cache,
 	})
 
-	sClient := newStatsdClient(conf)
+	/*sClient := newStatsdClient(conf)
 	if sClient != nil {
 		metrics, err = gubernator.NewStatsdMetrics(sClient)
 		checkErr(err, "while starting statsd metrics")
 		opts = append(opts, grpc.StatsHandler(metrics.GRPCStatsHandler()))
 		metrics.RegisterCacheStats(cache)
 		metrics.RegisterServerStats(guber)
-	}
+	}*/
 
 	checkErr(err, "while creating new gubernator instance")
 
@@ -133,57 +105,6 @@ func main() {
 			os.Exit(0)
 		}
 	}
-}
-
-func newStatsdClient(conf ServerConfig) *statsd.Client {
-	if conf.StatsdConfig.Endpoint == "" {
-		log.Info("Metrics config missing; metrics disabled")
-		return nil
-	}
-
-	if conf.StatsdConfig.Prefix == "" {
-		hostname, _ := os.Hostname()
-		normalizedHostname := strings.Replace(hostname, ".", "_", -1)
-		conf.StatsdConfig.Prefix = fmt.Sprintf("gubernator.%v.", normalizedHostname)
-	}
-
-	holster.SetDefault(&conf.StatsdConfig.Period.Duration, time.Second)
-
-	return statsd.NewClient(conf.StatsdConfig.Endpoint,
-		statsd.FlushInterval(conf.StatsdConfig.Period.Duration),
-		statsd.MetricPrefix(conf.StatsdConfig.Prefix),
-		statsd.Logger(log))
-}
-
-func loadConfig() (ServerConfig, error) {
-	var configFile string
-	var conf ServerConfig
-
-	flags := flag.NewFlagSet("gubernator", flag.ContinueOnError)
-	flags.StringVar(&configFile, "config", "", "yaml config file")
-	if err := flags.Parse(os.Args[1:]); err != nil {
-		return conf, err
-	}
-
-	if configFile == "" {
-		return conf, errors.New("config file is required; please provide a --config flag")
-	}
-	log.Infof("Loading config: %s", configFile)
-
-	fd, err := os.Open(configFile)
-	if err != nil {
-		return conf, fmt.Errorf("while opening config file: %s", err)
-	}
-
-	content, err := ioutil.ReadAll(fd)
-	if err != nil {
-		return conf, fmt.Errorf("while reading config file '%s': %s", configFile, err)
-	}
-
-	if err := yaml.Unmarshal(content, &conf); err != nil {
-		return conf, fmt.Errorf("while marshalling config file '%s': %s", configFile, err)
-	}
-	return conf, nil
 }
 
 func checkErr(err error, msg string) {

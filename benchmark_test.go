857e8deb86b79e536be3c69c0a65fe37a7c559cc
Shawn Poulson <spoulson@pathwire.com>
2022-01-05 16:28:32 -0500

    PIP-1490: Update all copyrights to current year.

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018-2019 Mailgun Technologies Inc
+Copyright 2018-2022 Mailgun Technologies Inc
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- logging/logging.go
+++ logging/logging.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018-2019 Mailgun Technologies Inc
+Copyright 2018-2022 Mailgun Technologies Inc
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package logging
 
 import (
 	"encoding/json"
+
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )

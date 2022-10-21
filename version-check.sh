2cc5e2c0e42097d3c0c4fc07be2273ed5b91a4d6
Shawn Poulson <spoulson@pathwire.com>
2022-10-21 15:52:55 -0400

    Fix version check.

--- version-check.sh
+++ version-check.sh
@@ -4,30 +4,36 @@
 # Strip leading 'v'.
 VERSION=$(git describe --tags $(git rev-list --tags --max-count=1) | sed -e 's/^v//')
 if [ -z "$VERSION" ]; then
-  >1 echo "Unable to determine version from git tags."
+  echo "Unable to determine version from git tags." >&1
   exit 1
 fi
 
-echo "Version: $VERSION"
+echo "Version tag: $VERSION"
+RETCODE=0
 
 # Check version file.
-grep "^$VERSION$" version || (
-  echo "version file mismatch: $VERSION <=> $(cat version)" >1
-  exit 1
-)
-echo 'version file OK'
+if [ ! "v$VERSION" = "$(cat version)" ]; then
+  echo "version file mismatch: v$VERSION <=> $(cat version)" >&1
+  RETCODE=1
+else
+  echo 'version file OK'
+fi
 
 # Check Helm chart.
 HELM_VERSION=$(yq .version charts/gubernator/Chart.yaml)
 if [ "$VERSION" != "$HELM_VERSION" ]; then
-  echo "Helm chart version mismatch: $VERSION <=> $HELM_VERSION" >1
-  exit 1
+  echo "Helm chart version mismatch: $VERSION <=> $HELM_VERSION" >&1
+  RETCODE=1
+else
+  echo 'Helm chart version OK'
 fi
-echo 'Helm chart version OK'
 
 HELM_APPVERSION=$(yq .appVersion charts/gubernator/Chart.yaml)
 if [ "$VERSION" != "$HELM_APPVERSION" ]; then
-  echo "Helm chart appVersion mismatch: $VERSION <=> $HELM_APPVERSION" >1
-  exit 1
+  echo "Helm chart appVersion mismatch: $VERSION <=> $HELM_APPVERSION" >&1
+  RETCODE=1
+else
+  echo 'Helm chart appVersion OK'
 fi
-echo 'Helm chart appVersion OK'
+
+exit $RETCODE

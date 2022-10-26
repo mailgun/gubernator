8cdcf81ac11e6099ed67ab9ef25ac0d5ff4db2de
Shawn Poulson <spoulson@pathwire.com>
2022-10-26 11:33:30 -0400

    Fix errors writing to STDERR.

--- version-check.sh
+++ version-check.sh
@@ -4,7 +4,7 @@
 # Strip leading 'v'.
 VERSION=$(git describe --tags $(git rev-list --tags --max-count=1) | sed -e 's/^v//')
 if [ -z "$VERSION" ]; then
-  echo "Unable to determine version from git tags." >&1
+  echo "Unable to determine version from git tags." >&2
   exit 1
 fi
 
@@ -13,7 +13,7 @@ RETCODE=0
 
 # Check version file.
 if [ ! "v$VERSION" = "$(cat version)" ]; then
-  echo "version file mismatch: v$VERSION <=> $(cat version)" >&1
+  echo "version file mismatch: v$VERSION <=> $(cat version)" >&2
   RETCODE=1
 else
   echo 'version file OK'
@@ -22,7 +22,7 @@ fi
 # Check Helm chart.
 HELM_VERSION=$(yq .version charts/gubernator/Chart.yaml)
 if [ "$VERSION" != "$HELM_VERSION" ]; then
-  echo "Helm chart version mismatch: $VERSION <=> $HELM_VERSION" >&1
+  echo "Helm chart version mismatch: $VERSION <=> $HELM_VERSION" >&2
   RETCODE=1
 else
   echo 'Helm chart version OK'
@@ -30,7 +30,7 @@ fi
 
 HELM_APPVERSION=$(yq .appVersion charts/gubernator/Chart.yaml)
 if [ "$VERSION" != "$HELM_APPVERSION" ]; then
-  echo "Helm chart appVersion mismatch: $VERSION <=> $HELM_APPVERSION" >&1
+  echo "Helm chart appVersion mismatch: $VERSION <=> $HELM_APPVERSION" >&2
   RETCODE=1
 else
   echo 'Helm chart appVersion OK'

91815788c989cd9cff4ec7846c87068b359eec34
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-02-19 21:24:55 -0300

    fix version of gateway

--- gubernator.pb.gw.go
+++ gubernator.pb.gw.go
@@ -35,7 +35,11 @@ func request_V1_GetRateLimits_0(ctx context.Context, marshaler runtime.Marshaler
 	var protoReq GetRateLimitsReq
 	var metadata runtime.ServerMetadata
 
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -48,7 +52,11 @@ func local_request_V1_GetRateLimits_0(ctx context.Context, marshaler runtime.Mar
 	var protoReq GetRateLimitsReq
 	var metadata runtime.ServerMetadata
 
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
